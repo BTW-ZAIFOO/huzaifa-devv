685d6b929b97e60d078c186300a17698549f6850
Muhsan Sattar <Muhsansattar97@gmail.com>
2025-05-27 20:01:48 +0500

    remove unused components and clean up Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,8 @@
 import React, { useContext } from "react";
-import Hero from "../components/Hero";
-import Instructor from "../components/Instructor";
-import Technologies from "../components/Technologies";
 import { toast } from "react-toastify";
 import axios from "axios";
 import { Context } from "../main";
 import { Link, Navigate } from "react-router-dom";
-import Footer from "../layout/Footer";
 
 const Home = () => {
   const { isAuthenticated, setIsAuthenticated, setUser, user, isAdmin } = useContext(Context);
@@ -60,12 +56,6 @@ const Home = () => {
             </div>
           </div>
         </nav>
-        <div className="pt-20">
-          <Hero />
-          <Instructor />
-          <Technologies />
-          <Footer />
-        </div>
       </div>
     </>
   );
